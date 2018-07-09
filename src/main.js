0b245cd7ba4dff6f49328a812ddf5bbbb774df71
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-07-09 11:12:14 -0400

    sacamos el chk

--- src/main.js
+++ src/main.js
@@ -4,28 +4,30 @@ boton.addEventListener('click', () => { /*Es igual a function ()*/
     document.getElementById('comment').value = '';
     const cont = document.getElementById('cont');
     const newComments = document.createElement('div');
-    const chck = document.createElement('input');
-    chck.type = 'checkbox';
+    
+    //corazon
     const heart = document.createElement('i');
     heart.classList.add('fa', 'fa-heart', 'heart');
+    //evento click corazon
+    heart.addEventListener('click', ()=> {
+      heart.classList.toggle('red');
+    })
+    
+    //Basura
     const trash = document.createElement('i');
     trash.classList.add('fa', 'fa-trash', 'trash');
+    //Evento click basura
+    trash.addEventListener('click', ()=> {
+      cont.removeChild(newComments);
+    })
+
+    //Crear p nuevo con comentario
     const contenedorElemento = document.createElement('p');
     let textNewComment = document.createTextNode(comments);
     contenedorElemento.appendChild(textNewComment);
-    newComments.appendChild(chck);
     newComments.appendChild(heart);
     newComments.appendChild(trash);
     newComments.appendChild(contenedorElemento);
     cont.appendChild(newComments);
 
-    heart.addEventListener('click', ()=> {
-        heart.classList.toggle('red');
-    })
-    trash.addEventListener('click', ()=> {
-        cont.removeChild(newComments);
-    })
-    chck.addEventListener('click', ()=> {
-        contenedorElemento.classList.toggle('strike-out');//linea horizontal sobre el texto
-    })
 }) 
