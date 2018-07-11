b0dc5a283eba4c6616b15b7960b3b6fcbaffadaf
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-07-11 12:58:39 -0400

    validador comentario

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,14 @@
-    //corazon
-    const heart = document.createElement('i');
-    heart.classList.add('fa', 'fa-heart', 'heart');
-    //evento click corazon
-    heart.addEventListener('click', ()=> {
-      heart.classList.toggle('red');
-    })
-    
-    //Basura
-    const trash = document.createElement('i');
-    trash.classList.add('fa', 'fa-trash', 'trash');
-    //Evento click basura
-    trash.addEventListener('click', ()=> {
-      cont.removeChild(newComments);
-    })
-
 const boton = document.getElementById('btn');
 boton.addEventListener('click', () => { /*Es igual a function ()*/
     let comments = document.getElementById('comment').value;
     document.getElementById('comment').value = '';
     const cont = document.getElementById('cont');
     const newComments = document.createElement('div');
+
+    if(comments.length === 0 || comments === null){
+      alert ('Debes ingresar un mensaje');
+      return false;
+    }
     
     //corazon
     const heart = document.createElement('i');
