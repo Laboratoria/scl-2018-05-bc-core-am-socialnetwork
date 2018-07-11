4a7ab45cbfcf722654651537ef08f9c390f281ad
Cote <majovardon@gmail.com>
2018-07-11 18:39:45 -0400

    Agregando sign in y sign up. Logout agregado

---
+++ src/js/main.js
@@ -0,0 +1,63 @@
+window.onload = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      //Si estamos logueados
+      loggedOut.style.display = "none";
+      loggedIn.style.display = "block";
+      console.log("User > " + JSON.stringify(user));
+    } else {
+      //No estamos logueados
+      loggedOut.style.display = "block";
+      loggedIn.style.display = "none";
+    }
+  });  
+};
+
+function signUp() {
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
+    .then(() => {
+      console.log("Usuario registrado");
+    })
+    .catch((error) => {
+      console.log("Error de firebase > " + error.code);
+      console.log("Error de firebase, mensaje > " + error.message);
+    });
+}
+
+function signIn() {
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+    .then(() => {
+      console.log("Usuario con login exitoso");
+    })
+    .catch((error) => {
+      console.log("Error de firebase > " + error.code);
+      console.log("Error de firebase, mensaje > " + error.message);
+    });
+}
+
+function logOut() {
+  firebase.auth().signOut()
+    .then(() => {
+      alert('¡Adiós, nos vemos pronto!');
+    })
+    .catch();
+}
+
+function loginFacebook() {
+  const provider = new firebase.auth.FacebookAuthProvider();
+  provider.setCustomParameters({
+    'display': 'popup'
+  });
+  firebase.auth().signInWithPopup(provider)
+    .then(() => {
+      console.log("Login con facebook");
+    })
+    .catch((error) => {
+      console.log("Error de firebase > " + error.code);
+      console.log("Error de firebase, mensaje > " + error.message);
+    });
+}
