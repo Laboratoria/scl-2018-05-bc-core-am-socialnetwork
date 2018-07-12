ba45880baf2078fb00ab318634be8bab5d568fc7
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-07-12 08:55:32 -0400

    Agregada funcionalidad para botón registrar

---
+++ src/assets/js/main.js
@@ -0,0 +1,6 @@
+const signUp = document.getElementById('btn_signUp');
+  
+signUp.addEventListener('click', () => {
+  document.getElementById('screen1').style.display = 'none';
+  document.getElementById('screen2').style.display = 'block';
+});
