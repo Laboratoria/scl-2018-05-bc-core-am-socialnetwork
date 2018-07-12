58eaa6debee410649f94bdbf70d0c9c9ae6195a4
Carolina Lastra <carolastra.cofre@gmail.com>
2018-07-12 12:44:54 -0400

    agregando menú lateral, html, js y css

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -37,6 +37,24 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+//Función del menú lateral
+function toggleMenu() { 
+  if (sideMenu.className.indexOf("menu_closed") >= 0) { 
+    openMenu();  
+  } else {
+    closeMenu(); 
+  }
+}
+
+function openMenu() {
+  sideMenu.classList.remove('menu_closed')
+  sideMenu.classList.add('menu_open');
+}
+
+function closeMenu() {
+  sideMenu.classList.add('menu_closed');
+  sideMenu.classList.remove('menu_open');
+}
 
 /*  
   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
