be33217a0e15c269fc2ed875d47ab00759ac1211
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-07-12 10:51:48 -0400

    Agregado modal de advertencia o aviso de responsabilidad (sin aviso) y cuadro de registro

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,12 @@
 const signUp = document.getElementById('btn_signUp');
+const createAccount = document.getElementById('btn_registrar');
   
 signUp.addEventListener('click', () => {
   document.getElementById('screen1').style.display = 'none';
   document.getElementById('screen2').style.display = 'block';
+});
+
+createAccount.addEventListener('click', () => {
+  document.getElementById('screen2').style.display = 'none';
+  console.log('Aquí se abrirá pantalla 3, para publicaciones de usuarios');
 });
