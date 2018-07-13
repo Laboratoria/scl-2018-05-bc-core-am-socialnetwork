629f4ee84d6ce275bbaceab4d3e508673e75486f
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-07-13 08:12:55 -0400

    arreglos del menú y perfil del usuario

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,3 +4,24 @@ $(document).ready(function() {
     $(this).toggleClass('open');
   });
 });
+
+// Cambiar foto de perfil
+$(document).ready(function() {
+  var readURL = function(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(event) {
+        $('.profile-pic').attr('src', event.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
+
+  $('.file-upload').on('change', function() {
+    readURL(this);
+  });
+
+  $('.upload-button').on('click', function() {
+    $('.file-upload').click();
+  });
+});
