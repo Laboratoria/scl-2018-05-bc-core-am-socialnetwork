bcab646eb3c85c19fffa7e0aa1e63dcb027616ba
Carolinadelb <carolinadelgadob@gmail.com>
2018-07-13 12:19:46 -0400

    con tab funcionando

--- src/assets/js/tab.js
+++ src/assets/js/tab.js
@@ -1,13 +1,5 @@
-//config de firebase
-var config = {
-  apiKey: "AIzaSyDDPfdzZ-OL-5U1ZFgYEJd40Di3WdR36Wc",
-  authDomain: "social-network-d7373.firebaseapp.com",
-  databaseURL: "https://social-network-d7373.firebaseio.com",
-  projectId: "social-network-d7373",
-  storageBucket: "social-network-d7373.appspot.com",
-  messagingSenderId: "188592192471"
-};
-firebase.initializeApp(config);
+
+
 //mensajes
 firebase.database().ref('messages')
         .limitToLast(20) // Filtro para no obtener todos los mensajes
