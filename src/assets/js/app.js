103df6f88ddfae519332084a55f133c52ec240ea
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-07-19 00:41:25 -0400

    cambios de una página a otra con funciones y dom, agregué función de cierre de menú al iniciar sesión con todos los métodos

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -39,28 +39,53 @@ function toShowScreen3(user) {
     document.getElementById('screen2').style.display = 'none';
     document.getElementById('screen3').style.display = 'block';
     document.getElementById('menuNav').style.visibility = 'visible';
+    closeMenu();
     let displayName = email;
   } else if (providerData[0].providerId === 'facebook.com') {
     document.getElementById('screen1').style.display = 'none';
     document.getElementById('screen2').style.display = 'none';
     document.getElementById('screen3').style.display = 'block';
     document.getElementById('menuNav').style.visibility = 'visible';
+    closeMenu();
   } else if (userGoogle[0].providerId === 'google.com') {
     document.getElementById('screen1').style.display = 'none';
     document.getElementById('screen2').style.display = 'none';
     document.getElementById('screen3').style.display = 'block';
+    document.getElementById('menuNav').style.visibility = 'visible';
+    closeMenu();
   }
 }
 
 function toShowScreenAlimentacion() {
   document.getElementById('screen3').style.display = 'none';
+  document.getElementById('screenCuidados').style.display = 'none';
+  document.getElementById('screenDirectorio').style.display = 'none';
   document.getElementById('screenAlimentacion').style.display = 'block';
   closeMenu();
 }
 
 function toShowScreenCuidados() {
+  document.getElementById('screen3').style.display = 'none';
+  document.getElementById('screenDirectorio').style.display = 'none';
   document.getElementById('screenAlimentacion').style.display = 'none';
   document.getElementById('screenCuidados').style.display = 'block';
+  closeMenu();
+}
+
+function toShowScreenDirectorio() {
+  document.getElementById('screen3').style.display = 'none';
+  document.getElementById('screenAlimentacion').style.display = 'none';
+  document.getElementById('screenCuidados').style.display = 'none';
+  document.getElementById('screenDirectorio').style.display = 'block';
+  closeMenu();
+}
+
+function toShowScreenAdopciones() {
+  document.getElementById('screen3').style.display = 'none';
+  document.getElementById('screenAlimentacion').style.display = 'none';
+  document.getElementById('screenCuidados').style.display = 'none';
+  document.getElementById('screenDirectorio').style.display = 'none';
+  document.getElementById('screenAdopciones').style.display = 'block';
   closeMenu();
 }
 // Sección registrar
@@ -132,6 +157,10 @@ function logout() {
     .then(()=> {
       console.log('Cerraste sesión');
       document.getElementById('menuNav').style.visibility = 'hidden';
+      document.getElementById('screen3').style.display = 'none';
+      document.getElementById('screenDirectorio').style.display = 'none';
+      document.getElementById('screenAlimentacion').style.display = 'none';
+      document.getElementById('screenCuidados').style.display = 'none';
     })
     .catch();
 }
